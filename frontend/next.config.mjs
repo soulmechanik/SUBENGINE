c2bf1866bdce546a02db89aeb72963048ee4a6a9
Owa Rotimi Emmanuel <owarotimiadebowale@gmail.com>
2025-06-20 18:02:38 +0100

    next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_BACKEND_API_URL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+  },
+};
 
 export default nextConfig;
